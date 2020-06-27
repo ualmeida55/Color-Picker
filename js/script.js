07973116aa76a5dc79f3e5742f9d0366509c3865
ualmeida55 <ualmeida55@gmail.com>
2020-06-26 22:35:43 -0300

    Add Color Picker files

---
+++ js/script.js
@@ -0,0 +1,38 @@
+function red() {
+    document.getElementById("showcolor").style.backgroundColor="#c72c41";
+}
+
+function green() {
+    document.getElementById("showcolor").style.backgroundColor="#06623b";
+}
+
+function blue() {
+    document.getElementById("showcolor").style.backgroundColor="#035aa6";
+}
+
+function cyan() {
+    document.getElementById("showcolor").style.backgroundColor="#12cad6";
+}
+
+function magenta() {
+    document.getElementById("showcolor").style.backgroundColor="#9C0063";
+}
+
+function yellow() {
+    document.getElementById("showcolor").style.backgroundColor="#f6d743";
+}
+
+function reset() {
+    document.getElementById("showcolor").style.backgroundColor="#000000";
+}
+
+
+
+/*
+    Red: ee4540 c72c41
+    Blue: 035aa6 120136
+    Green: 649d66 06623b
+    Cyan: 12cad6 0fabbc
+    Magenta: D693BD 9C0063
+    Yellow: f6f578 f6d743
+*/
